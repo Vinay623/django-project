b45c29441ffafa09df6d1fb32706f651d6914547
Vinay623 <64736846+Vinay623@users.noreply.github.com>
2026-04-18 22:13:46 +0930

    Updated ADR properly

--- enrolment/admin.py
+++ enrolment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Course, Enrolment
+from .models import Student, Course, Enrolment 
 
 admin.site.register(Student)
 admin.site.register(Course)
